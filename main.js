3328348e1b81c4d60063b9cb400c7ca36c5fb820
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-11-15 11:44:51 -0500

    crete cards with names

---
+++ main.js
@@ -0,0 +1,20 @@
+const customerContainer = document.querySelector("#customers");
+// grab the customer container
+
+console.log(customerContainer);
+
+// loop through the customer objects and build html for each one
+for (let customer of customers) {
+  // create a card for the customer
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add("small");
+
+  let name = document.createElement("div");
+  // create a div for name and apply the materialize class to it
+  name.classList.add("card-title");
+  name.innerText = `${customer.name.first} ${customer.name.last}`;
+  card.appendChild(name);
+  // add the name to the card
+  customerContainer.appendChild(card);
+}
